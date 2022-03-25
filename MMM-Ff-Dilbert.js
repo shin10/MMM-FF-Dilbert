c84dac79e8faf318d0fe0c3dfc7c68852e9325bb
Michael Trenkler <shin10@gmx.de>
2022-03-25 18:56:25 +0100

    mv styles/add scss

--- MMM-Ff-Dilbert.js
+++ MMM-Ff-Dilbert.js
@@ -98,7 +98,7 @@ Module.register("MMM-Ff-Dilbert", {
   },
 
   getStyles: function () {
-    return ["MMM-Ff-Dilbert.css"];
+    return [this.file("./styles/MMM-Ff-Dilbert.css")];
   },
 
   getHeader: function () {
