c2f2df36b43f7506e82da6bcb6df63222a22fb2d
Michael Trenkler <shin10@gmx.de>
2022-03-29 22:52:35 +0200

    capture event w/o config

--- MMM-FF-Dilbert.js
+++ MMM-FF-Dilbert.js
@@ -113,7 +113,8 @@ Module.register("MMM-FF-Dilbert", {
   },
 
   socketNotificationReceived: function (notification, payload) {
-    if (payload.config?.moduleId !== this.config.moduleId) return;
+    if (!payload.config || payload.config.moduleId !== this.config.moduleId)
+      return;
     switch (notification) {
       case "ERROR":
         this.error = payload;
